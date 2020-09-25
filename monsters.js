8a530549109cab2cb44baca5860ba6d1e29b3ef9
Brock Ellis <brock@sharproot.com>
2020-09-24 20:59:10 -0500

    Add files via upload

---
+++ monsters.js
@@ -0,0 +1,17 @@
+var listOfMonsters = [
+	{
+		name: "Angry Otter",
+		hp: 8,
+		damage: 2
+	},
+	{
+		name: "Mountain Troll",
+		hp: 35,
+		damage: 13
+	},
+	{
+		name: "Snake King",
+		hp: 72,
+		damage: 25
+	}
+]
